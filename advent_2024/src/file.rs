cc58f29afd6137004715b53cacaf3089e2191e63
Yeeloman <yami.onlyme@gmail.com>
2024-12-03 14:59:03 +0100

    rm comment

--- advent_2024/src/file.rs
+++ advent_2024/src/file.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::{self, BufRead, Read};
 
 pub fn load_content(path: &str) -> io::Result<String> {
-    // const PATH: &str = "src/input/in_3";
     let mut input = String::new();
     let mut file = File::open(path)?;
     file.read_to_string(&mut input)?;
