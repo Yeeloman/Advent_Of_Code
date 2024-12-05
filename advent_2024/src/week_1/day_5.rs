e0a665686ee64c050e9e79f4de1dc24b6dd92eca
Yeeloman <yami.onlyme@gmail.com>
2024-12-06 00:53:16 +0100

    getting a clean input

--- advent_2024/src/week_1/day_5.rs
+++ advent_2024/src/week_1/day_5.rs
@@ -1,4 +1,5 @@
 use crate::file;
+use regex::Regex;
 
 const PATH: &str = "src/inputs/test";
 
@@ -7,14 +8,16 @@ pub fn main() -> std::io::Result<()> {
     let mut input = file::load_content(PATH)?;
 
     let (rules, updates) = process_file(&input);
+
     file::print_challenges(5, 0, 0);
     Ok(())
 }
 
-fn process_file<'a>(input: &'a str) -> (Vec<&'a str>, Vec<&'a str>) {
+fn process_file<'a>(input: &'a str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
     let mut empty_line: bool = false;
-    let mut rules: Vec<&'a str> = Vec::new();
-    let mut updates: Vec<&'a str> = Vec::new();
+    let mut rules: Vec<(i32, i32)> = Vec::new();
+    let mut updates: Vec<Vec<i32>> = Vec::new();
+    let re: Regex = Regex::new(r"(\d+)\|(\d+)").unwrap();
 
     for line in input.lines() {
         if line.trim().is_empty() {
@@ -22,9 +25,20 @@ fn process_file<'a>(input: &'a str) -> (Vec<&'a str>, Vec<&'a str>) {
             continue;
         }
         if empty_line {
-            updates.push(line);
+            let nums = line.split(",").map(|x| x.parse::<i32>().unwrap());
+            let mut update = Vec::new();
+            for n in nums {
+                update.push(n);
+            }
+            updates.push(update);
+            // println!("{:?}", updates)
         } else {
-            rules.push(line);
+            if let Some(cap) = re.captures(line) {
+                let n_1 = cap.get(1).unwrap().as_str().parse::<i32>().ok().unwrap();
+                let n_2 = cap.get(2).unwrap().as_str().parse::<i32>().ok().unwrap();
+                // println!("{:?}", (n_1, n_2));
+                rules.push((n_1, n_2));
+            }
         }
     }
     (rules, updates)
