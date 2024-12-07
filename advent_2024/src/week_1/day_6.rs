76ffd742bf1cd70342da754e27c1af7b034be946
Yeeloman <yami.onlyme@gmail.com>
2024-12-07 01:59:40 +0100

    some refactoring

--- advent_2024/src/week_1/day_6.rs
+++ advent_2024/src/week_1/day_6.rs
@@ -24,11 +24,11 @@ impl Direction {
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug)]
 struct Guard {
     orientation: Direction,
     position: (usize, usize),
+    history: Vec<(usize, usize)>,
     visited: HashSet<(usize, usize)>,
 }
 
@@ -48,13 +48,28 @@ impl Guard {
         Guard {
             orientation,
             position: (i, j),
+            history: vec![(i, j)],
             visited,
         }
     }
 
     fn walk(&mut self, next: (usize, usize)) {
         self.position = (next.0, next.1);
+        self.history.push(self.position);
         self.visited.insert(self.position);
+    }
+
+    fn is_stuck(&self) -> bool {
+        let history_len = self.history.len();
+        let loop_len = 4;
+
+        if history_len < loop_len * 2 {
+            return false;
+        }
+        let recent_path = &self.history[history_len - loop_len..];
+        let earlier_path = &self.history[history_len - loop_len * 2..history_len - loop_len];
+
+        recent_path == earlier_path
     }
 
     fn turn(&mut self) {
@@ -73,26 +88,12 @@ impl Guard {
 
 #[allow(unused_variables, unused_mut)]
 pub fn main() -> std::io::Result<()> {
-    let mut in_patrol: bool = true;
     let mut answer_2 = 0;
     let mut input = file::load_content(PATH)?;
 
     let (mut grd, map) = init_setup(input);
-    while in_patrol {
-        let crnt_pos = grd.position;
-        let mv_dir = grd.orientation.vector();
-        let (new_i, new_j) = (crnt_pos.0 as i32+ mv_dir.0, crnt_pos.1 as i32+ mv_dir.1);
-        if new_i < 0 || new_i > map.len() as i32 - 1|| new_j < 0 || new_j > map[0].len() as i32 - 1 {
-            in_patrol = false;
-            continue;
-        }
 
-        if map[new_i as usize][new_j as usize] == "#".chars().next().unwrap() {
-            grd.turn();
-        } else {
-            grd.walk((new_i as usize, new_j as usize));
-        }
-    }
+    part_1::main(&mut grd, map);
 
     let answer_1 = grd.path_count() as i32;
     file::print_challenges(DAY, answer_1, answer_2);
@@ -121,9 +122,30 @@ fn init_setup(input: String) -> (Guard, Vec<Vec<char>>) {
 }
 
 mod part_1 {
+    use crate::week_1::day_6::Guard;
+
     #[allow(dead_code)]
-    pub fn main() {
-        todo!()
+    pub fn main(grd: &mut Guard, map: Vec<Vec<char>>) {
+        let mut in_patrol: bool = true;
+        while in_patrol {
+            let crnt_pos = grd.position;
+            let mv_dir = grd.orientation.vector();
+            let (new_i, new_j) = (crnt_pos.0 as i32 + mv_dir.0, crnt_pos.1 as i32 + mv_dir.1);
+            if new_i < 0
+                || new_i > map.len() as i32 - 1
+                || new_j < 0
+                || new_j > map[0].len() as i32 - 1
+            {
+                in_patrol = false;
+                continue;
+            }
+
+            if map[new_i as usize][new_j as usize] == "#".chars().next().unwrap() {
+                grd.turn();
+            } else {
+                grd.walk((new_i as usize, new_j as usize));
+            }
+        }
     }
 }
 
