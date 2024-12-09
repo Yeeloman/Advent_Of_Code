efbb19427225d437890e69b0f78289f2c0093428
Yeeloman <yami.onlyme@gmail.com>
2024-12-09 01:47:20 +0100

    part 2 day 7

--- advent_2024/src/week_1/mod.rs
+++ advent_2024/src/week_1/mod.rs
@@ -8,15 +8,17 @@ mod day_7;
 
 #[allow(unused_must_use)]
 pub fn challenges(done: Option<bool>) {
+    let mut print = false;
     if let Some(done) = done {
-        if done {
-            day_1::main();
-            day_2::main();
-            day_3::main();
-            day_4::main();
-            day_5::main();
-            day_6::main();
-            day_7::main();
-        }
+        print = done
+    }
+    if print {
+        day_1::main();
+        day_2::main();
+        day_3::main();
+        day_4::main();
+        day_5::main();
+        day_6::main();
+        day_7::main();
     }
 }
---
+++ advent_2024/src/week_2/mod.rs
@@ -0,0 +1,11 @@
+
+#[allow(unused_must_use)]
+pub fn challenges(done: Option<bool>) {
+    let mut print: bool = false;
+    if let Some(done) = done {
+        print = done
+    }
+    if print {
+
+    }
+}
